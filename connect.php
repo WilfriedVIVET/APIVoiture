c23cdf24dd382bd6d35088eb3e954e6cab724640
WilfriedVIVET <vivetlec@outlook.fr>
2024-02-02 11:16:19 +0100

    test

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: eu-az-sql-serv1.database.windows.net");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST");
